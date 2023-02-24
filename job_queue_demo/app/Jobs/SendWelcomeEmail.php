0197ad864ab87df9f1ee85150a70b68c6bdada2c
HiteshRupavatiya <hitesh@zignuts.com>
2023-02-24 18:24:34 +0530

    SendWelcomeEmail job added for send email without wait for loading the page using handle method call the Mail class

--- job_queue_demo/app/Jobs/SendWelcomeEmail.php
+++ job_queue_demo/app/Jobs/SendWelcomeEmail.php
@@ -2,25 +2,29 @@
 
 namespace App\Jobs;
 
+use App\Mail\WelcomeEmail;
 use Illuminate\Bus\Queueable;
 use Illuminate\Contracts\Queue\ShouldBeUnique;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Bus\Dispatchable;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Queue\SerializesModels;
+use Illuminate\Support\Facades\Mail;
 
 class SendWelcomeEmail implements ShouldQueue
 {
     use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    public $user;
 
     /**
      * Create a new job instance.
      *
      * @return void
      */
-    public function __construct()
+    public function __construct($user)
     {
-        //
+        $this->user = $user;
     }
 
     /**
@@ -30,6 +34,6 @@ class SendWelcomeEmail implements ShouldQueue
      */
     public function handle()
     {
-        //
+        Mail::to($this->user->email)->send(new WelcomeEmail($this->user));
     }
 }
